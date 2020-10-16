409fa9e1baea0fdfb7959c169b4184d7fe0a498e
Rusev11 <ivaylo.rusev@outlook.com>
2020-10-16 12:24:43 +0300

    Update

--- JS Web/JS Back-End/05. EXPRESSJS AND TEMPLATING/Cubicle v2 - Part 2 - MongoDB and Mongoose/config/config.js
+++ JS Web/JS Back-End/05. EXPRESSJS AND TEMPLATING/Cubicle v2 - Part 2 - MongoDB and Mongoose/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
         port: process.env.PORT || 5000,
         databaseUrl: process.env.DATABASE_URL
-        // `mongodb+srv://rusev:${`123qwe`}@cluster.bsqnt.mongodb.net/cubicle?retryWrites=true&w=majority`
+        
     },
     production: {}
 };  
